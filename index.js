2b856329e873fe8649f2013adebdd5c9d2bd2a5b
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-03-03 21:45:42 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const total= 0;
+// Amolat: Using ananoymous funtion 
+const totalBatteries= batteryBatches.reduce(function (init, current){
+    return init+current; 
+},0);
+
+
